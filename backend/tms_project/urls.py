d16b4d5b04fdfc0e27f167fb96fe255ceeb82dc8
Bebin-dotm <bbebinr@gmail.com>
2026-07-28 21:56:35 +0530

    Beta v2.75.3

--- backend/tms_project/urls.py
+++ backend/tms_project/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 from rest_framework_simplejwt.views import TokenRefreshView
 from core.views import MyTokenObtainPairView
 
@@ -14,3 +15,8 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if not settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
